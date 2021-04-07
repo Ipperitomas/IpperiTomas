6ecbe665216a2cb040311d3dff5e1fb9334a803b
Ipperitomas <ipperitomas@gmail.com>
2021-04-07 19:56:12 -0300

    Migrates

---
+++ resources/views/newview.blade.php
@@ -0,0 +1,16 @@
+
+
+@extends('layouts.app')
+
+@section('content')
+       <div class="row">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="title"> Card Example </h3>
+                </div>
+                <div class="card-body">
+                    <h3>Body</h3>
+                </div>
+            </div>
+       </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludar', function () {
+    return view('newview');
+});
+
+Route::get('/admin', function () {
+    return view('home');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
